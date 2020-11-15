bb90aa082325f6e46666ab2c4be6c4175e682997
zakzhu <zakzhu@outlook.com>
2020-11-16 03:37:03 +0800

    develop module edison/yaml_conf.py

---
+++ edison/yaml_conf.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Author: Zak Zhu
+# @Date:   2020-11-16 01:54:15
+# @Last Modified by:   Zak Zhu
+# @Last Modified time: 2020-11-16 03:26:01
+
+import yaml
+
+
+def get_data(file):
+    with open(file, mode="r", encoding="utf-8") as f:
+        data = yaml.safe_load(f)
+    return data
+
+
+def test():
+    data = get_data("/root/Programs/python/v3/cmd-edison/flag.yml")
+    print(data)
+
+
+if __name__ == "__main__":
+    test()
---
+++ main.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+# @Author: Zak Zhu
+# @Date:   2020-11-16 03:36:22
+# @Last Modified by:   Zak Zhu
+# @Last Modified time: 2020-11-16 03:36:24
+
+
+def main():
+    print("Hello, World!")
+
+
+if __name__ == "__main__":
+    main()
