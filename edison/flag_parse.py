f7819d324322ea69cc87e0286503b6f185759bac
zakzhu <zakzhu@outlook.com>
2020-11-18 17:56:48 +0800

    developing

--- edison/flag_parse.py
+++ edison/flag_parse.py
@@ -3,16 +3,11 @@
 # @Author: Zak Zhu
 # @Date:   2020-11-16 11:01:19
 # @Last Modified by:   Zak Zhu
-# @Last Modified time: 2020-11-18 16:05:48
+# @Last Modified time: 2020-11-18 17:55:15
 
 import argparse
 
-# >>> parser.add_argument('--foo')
-# _StoreAction(option_strings=['--foo'], dest='foo', nargs=None, const=None, default=None, type=None, choices=None, help=None, metavar=None)
-
 # class argparse.ArgumentParser(description=None)
-
-# ArgumentParser.add_argument(name or flags...[, action][, nargs][, const][, default][, type][, choices][, required][, help][, metavar][, dest])
 
 # ArgumentParser.add_argument(
 # name or flags...
@@ -20,19 +15,26 @@ import argparse
 # [, choices]
 # [, required]
 # [, help]
-# [, metavar])
 
 # parser.exit(status=rc)
 
 
-def parser(required_flag, optional_flag, help_msg, description, ver):
-    parser = argparse.ArgumentParser(description=description)
-    vers = '%(progs)s {}'.format(ver)
+
+def parser(flag_dict):
+    parser = argparse.ArgumentParser(description=flag_dict['description'])
+    vers = '%(progs)s {}'.format(flag_dict['version'])
     parser.add_argument('--version', action='version', version='%s' % vers)
+    for flag in flag_dict['flags']:
+        if flag.required:
+            
+
+        else:
+    # add boolean type arguments
     parser.add_argument(
         "--%s" % ,
         help=help_msg,
     )
+    # add string type arguments 
 
 
 def main():
