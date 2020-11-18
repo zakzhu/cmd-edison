3d3c1c933f0688d55da805137861505978b55d73
zakzhu <zakzhu@outlook.com>
2020-11-18 19:33:18 +0800

    developing

--- edison/flag_parse.py
+++ edison/flag_parse.py
@@ -3,7 +3,7 @@
 # @Author: Zak Zhu
 # @Date:   2020-11-16 11:01:19
 # @Last Modified by:   Zak Zhu
-# @Last Modified time: 2020-11-18 17:55:15
+# @Last Modified time: 2020-11-18 19:10:33
 
 import argparse
 
@@ -24,17 +24,60 @@ def parser(flag_dict):
     parser = argparse.ArgumentParser(description=flag_dict['description'])
     vers = '%(progs)s {}'.format(flag_dict['version'])
     parser.add_argument('--version', action='version', version='%s' % vers)
+    parser.add_argument('--quiet', action='store_true', help='')
+    def add_flag(is_required=True):
+        try:
+            if flags['type'] == 'bool':
+                parser.add_argument(
+                    '--%s' % flag['name'],
+                    type=bool,
+                    choices=[True, False],
+                    required=is_required,
+                    help=flag['help']
+                )
+        except KeyError:
+            try:
+                parser.add_argument(
+                    '--%s' % flag['name'],
+                    choices=flag['choices'],
+                    required=is_required,
+                    help=flag['help']
+                )
+            except KeyError:
+                parser.add_argument(
+                    '--%s' % flag['name'],
+                    required=is_required,
+                    help=flag['help']
+                )        
+
+
     for flag in flag_dict['flags']:
         if flag.required:
-            
-
+            try:
+                if flags['type'] == 'bool':
+                    parser.add_argument(
+                        '--%s' % flag['name'],
+                        type=bool,
+                        choices=[True, False],
+                        required=True,
+                        help=flag['help']
+                    )
+            except KeyError:
+                try:
+                    parser.add_argument(
+                        '--%s' % flag['name'],
+                        choices=flag['choices'],
+                        required=True,
+                        help=flag['help']
+                    )
+                except KeyError:
+                    parser.add_argument(
+                        '--%s' % flag['name'],
+                        required=True,
+                        help=flag['help']
+                    )
         else:
-    # add boolean type arguments
-    parser.add_argument(
-        "--%s" % ,
-        help=help_msg,
-    )
-    # add string type arguments 
+
 
 
 def main():
