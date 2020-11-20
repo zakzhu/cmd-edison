4269545f6b5ca0874fedf042fef8dbe29ebda630
zakzhu <zakzhu@outlook.com>
2020-11-20 09:16:41 +0800

    developing

--- edison/flag.py
+++ edison/flag.py
@@ -3,87 +3,70 @@
 # @Author: Zak Zhu
 # @Date:   2020-11-16 11:01:19
 # @Last Modified by:   Zak Zhu
-# @Last Modified time: 2020-11-18 22:45:32
+# @Last Modified time: 2020-11-20 09:15:19
 
 import argparse
 
-from . import yaml_conf
 
-# class argparse.ArgumentParser(description=None)
+def parse(flag_dict):
+    parser = argparse.ArgumentParser(description=flag_dict["description"])
+    vers = "%(progs)s {}".format(flag_dict["version"])
+    parser.add_argument("--version", action="version", version="%s" % vers)
+    parser.add_argument(
+        "--quiet", action="store_true", help="suppress non-error messages"
+    )
+    parser.add_argument(
+        "-v", "--verbose", action="count", help="verbose mode (-vv for more)"
+    )
 
-# ArgumentParser.add_argument(
-# name or flags...
-# [, type]
-# [, choices]
-# [, required]
-# [, help]
-
-# parser.exit(status=rc)
-
-
-
-def parser(flag_dict):
-    parser = argparse.ArgumentParser(description=flag_dict['description'])
-    vers = '%(progs)s {}'.format(flag_dict['version'])
-    parser.add_argument('--version', action='version', version='%s' % vers)
-    parser.add_argument('--quiet', action='store_true', help='')
-    def add_flag(is_required=True):
+    def add_flag(flag, is_required=True):
         try:
-            if flags['type'] == 'bool':
+            if flag["type"] == "bool":
                 parser.add_argument(
-                    '--%s' % flag['name'],
+                    "--%s" % flag["name"],
                     type=bool,
                     choices=[True, False],
                     required=is_required,
-                    help=flag['help']
+                    help=flag["help"],
                 )
         except KeyError:
             try:
                 parser.add_argument(
-                    '--%s' % flag['name'],
-                    choices=flag['choices'],
+                    "--%s" % flag["name"],
+                    choices=flag["choices"],
                     required=is_required,
-                    help=flag['help']
+                    help=flag["help"],
                 )
             except KeyError:
                 parser.add_argument(
-                    '--%s' % flag['name'],
+                    "--%s" % flag["name"],
                     required=is_required,
-                    help=flag['help']
-                )        
+                    help=flag["help"],
+                )
 
-
-    for flag in flag_dict['flags']:
-        if flag.required:
-            try:
-                if flags['type'] == 'bool':
-                    parser.add_argument(
-                        '--%s' % flag['name'],
-                        type=bool,
-                        choices=[True, False],
-                        required=True,
-                        help=flag['help']
-                    )
-            except KeyError:
-                try:
-                    parser.add_argument(
-                        '--%s' % flag['name'],
-                        choices=flag['choices'],
-                        required=True,
-                        help=flag['help']
-                    )
-                except KeyError:
-                    parser.add_argument(
-                        '--%s' % flag['name'],
-                        required=True,
-                        help=flag['help']
-                    )
+    for flag in flag_dict["flags"]:
+        flag = list(flag.values())[0]
+        if flag["required"]:
+            add_flag(flag)
         else:
+            add_flag(flag, False)
 
+    return parser
 
 
 def main():
-    print("Hello, World!")
+    import yaml_conf
+
+    file = "/root/Programs/python/v3/cmd-edison/flag.yml"
+    flag_dict = yaml_conf.get_data(file)
+    # print(flag_dict["flags"])
+    # for flag in flag_dict["flags"]:
+    #     print(list(flag.values())[0]["name"])
+    #     # for i in v:
+    #     # print(i)
+    #     # print(flag.values()[0]["name"])
+    parser = parse(flag_dict)
+    parser.print_help()
 
 
 if __name__ == "__main__":
